a522dfb62de6c755e154254ffd8d1f86fdea688e
Bryson Alexander Bayer <726082@calbaptist.edu>
2023-11-14 15:59:10 -0800

    Firebase Update 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);                                          
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
@@ -14,6 +17,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
 class HomeScreen extends StatelessWidget{
   @override
